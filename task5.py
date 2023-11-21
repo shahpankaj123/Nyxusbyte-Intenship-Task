3de0eb6a93e9171cf83006121c2679bf7e4c4019
Aaryan Shah <aaryanshah615@gmail.com>
2023-11-21 20:27:07 +0545

    5 task

---
+++ task5.py
@@ -0,0 +1,25 @@
+print("simple calculator")
+
+num1=int(input("enter any number"))
+num2=int(input("enter any number"))
+res=0
+
+
+print("please the choose the operation number to perform task")
+print("1.Addition")
+print("2.Substraction")
+print("3.Multiplication")
+print("4.Division")
+choice=int(input())
+
+if choice==1:
+    res=num1+num2
+elif choice==2:
+    res=num1-num2
+
+elif choice==3:
+    res=num1*num2
+elif choice ==4:
+    res =num1/num2
+
+print("your result:",res)
