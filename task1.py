1edcbb9b4cb27aa6a17c346299bc23aef339be58
Aaryan Shah <aaryanshah615@gmail.com>
2023-11-21 19:55:43 +0545

    first commit

---
+++ task1.py
@@ -0,0 +1,7 @@
+num=int(input("enter any number"))
+
+for i in range(1,num+1):
+    if i % 2 ==0:
+        print(i,"is even number")
+    else:
+        print(i,"is odd number")    
---
+++ task2.py
@@ -0,0 +1,18 @@
+import random
+
+print("Guessing game")
+random_number=random.randint(1,100)
+print(random_number)
+i=3
+while(i>0):
+    print("Plaese guess any number between 1 to 100")
+    num=int(input())
+    if num==random_number:
+        print("congrats you won ")
+    if num>random_number:
+        print("your guessed number is greater than actual number ")  
+    if num <random_number:
+        print("your guessed number is lower than actual number ") 
+       
+    i-=1
+    print("you have left trial",i) 
