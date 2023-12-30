cde28273b8b1a85ea3b5e7597c7c0462394bac5f
Aaryan Shah <aaryanshah615@gmail.com>
2023-12-30 16:30:24 +0545

    bookstore

--- Django_Task/Task_7/Blog_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BlogAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Blog_app'
--- Django_Task/Task_7/account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import Signup,Login,Logout
-
-urlpatterns = [
-    path('',Signup,name='signup'),
-    path('Login/',Login,name='Login'),
-    path('Logout',Logout,name='Logout')
-]
